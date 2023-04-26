f16dcccad48c74197341fa2edeac9d6271430f99
renaskigi <dev.renataprado@gmail.com>
2023-04-26 16:58:50 -0300

    Factor log component

--- components/FactorLog.js
+++ components/FactorLog.js
@@ -1,43 +1,59 @@
 import {StyleSheet, Text, View } from 'react-native';
 import { LinearProgress } from '@rneui/themed';
+import { factorSelector } from '../models/Factors';
+import Colors from '../contants/colors'
 
-function FactorLog({ id='title', selectedRate=0.6, selectedChips=['um', 'dois', 'vinte e quatro'] }) {
+
+function FactorLog( { id='title', selectedRate=0.6, selectedChips=['um', 'dois', 'vinte e quatro'] }) {
+  const Factor = factorSelector(id);
+  const decimalRate = Factor.calculateDecimalRate(selectedRate);
+  const backgroundColor = Colors[id+"Primary"];
 
   return (
-    <View style={styles.rootContainer}>
-      <Text style={styles.title}>{id}</Text>
+    <View style={[styles.rootContainer, {backgroundColor: backgroundColor}]}>
+      <Text style={styles.title}>{Factor.name}</Text>
       <View style={{width: '75%'}}>
         <LinearProgress 
-          style={{ marginVertical: 10, height: 10, borderRadius: 10 }}
-          value={selectedRate}
+          style={styles.linearProgress}
+          value={decimalRate}
+          color={Colors[id+"Darker"]}
+          trackColor='#959595'
           variant="determinate"
         />
         <View style={styles.chipsContainer}>
-         <Text>{selectedChips.join(", ")}</Text>
+         <Text style={{color: 'white'}}>{selectedChips.join(", ")}</Text>
         </View>
       </View>
     </View>
   );
 }
 
-export default FactorLog;
-
 const styles = StyleSheet.create({
   rootContainer:{
     height: 80,
-    backgroundColor: 'red',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: 10
+    padding: 10,
   },
   title: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 12,
-    alignSelf: 'center'
+    alignSelf: 'center',
+    color: 'white'
+  },
+  linearProgress: {
+    marginVertical: 10, 
+    height: 10, 
+    borderRadius: 10,
+
   },
   chipsContainer: {
-    flexDirection: 'row'
+    flexDirection: 'row',
   }
 });
+
+export default FactorLog;
+
+
